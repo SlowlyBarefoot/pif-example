83d095474cbda87837b05f1dd9b411f5b9b15006
SlowlyBarefoot <joon0323@naver.com>
2020-11-03 22:25:30 +0900

    Arduino Nano 보드에 Keypad 제어 기능 예제 추가.
    Added example of Keypad control to Arduino Nano board.

---
+++ arduino_nano/exKeypad1/exKeypad1.cpp
@@ -0,0 +1,139 @@
+// Do not remove the include below
+#include <MsTimer2.h>
+
+#include "exKeypad1.h"
+
+#include "pifKeypad.h"
+#include "pifLog.h"
+#include "pifPulse.h"
+
+
+#define PIN_LED_L				13
+
+#define PULSE_COUNT         	1
+#define PULSE_ITEM_COUNT    	1
+#define TASK_COUNT              2
+
+
+static PIF_stPulse *g_pstTimer1ms = NULL;
+
+const uint8_t ROWS = 4;    // 행(rows) 개수
+const uint8_t COLS = 4;    // 열(columns) 개수
+const char keys[ROWS * COLS] = {
+	'1', '2', '3', 'A',
+	'4', '5', '6', 'B',
+	'7', '8', '9', 'C',
+	'*', '0', '#', 'D'
+};
+
+uint8_t rowPins[ROWS] = { 6, 7, 8, 9 };   // R1, R2, R3, R4 단자가 연결된 아두이노 핀 번호
+uint8_t colPins[COLS] = { 5, 4, 3, 2 };   // C1, C2, C3, C4 단자가 연결된 아두이노 핀 번호
+
+
+static void _actLogPrint(char *pcString)
+{
+	Serial.print(pcString);
+}
+
+static void _actLedToggle(PIF_stTask *pstTask)
+{
+	static BOOL sw = LOW;
+
+	(void)pstTask;
+
+	digitalWrite(PIN_LED_L, sw);
+	sw ^= 1;
+}
+
+static void _actKeypadAcquire(uint16_t *pusState)
+{
+	int r, c;
+
+	for (c = 0; c < COLS; c++) {
+		pinMode(colPins[c], OUTPUT);
+		digitalWrite(colPins[c], LOW);
+		for (r = 0; r < ROWS; r++) {
+			if (!digitalRead(rowPins[r])) {
+				pusState[r] |= 1UL << c;
+			}
+			else {
+				pusState[r] &= ~(1UL << c);
+			}
+		}
+		digitalWrite(colPins[c], HIGH);
+		pinMode(colPins[c],INPUT);
+	}
+}
+
+static void _evtKeypadPressed(char cChar)
+{
+	pifLog_Printf(LT_enInfo, "Keypad:Pressed %c", cChar);
+}
+
+static void _evtKeypadReleased(char cChar, uint32_t unOnTime)
+{
+	pifLog_Printf(LT_enInfo, "Keypad:Released %c T:%lu", cChar, unOnTime);
+}
+
+static void _evtKeypadLongReleased(char cChar, uint32_t unOnTime)
+{
+	pifLog_Printf(LT_enInfo, "Keypad:LongReleased %c T:%lu", cChar, unOnTime);
+}
+
+static void _evtKeypadDoublePressed(char cChar)
+{
+	pifLog_Printf(LT_enInfo, "Keypad:DoublePressed %c", cChar);
+}
+
+void sysTickHook()
+{
+	pif_sigTimer1ms();
+
+	pifPulse_sigTick(g_pstTimer1ms);
+}
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+	int r;
+	PIF_stKeypad *pstKeypad;
+
+	pinMode(PIN_LED_L, OUTPUT);
+	for (r = 0; r < ROWS; r++) {
+		pinMode(rowPins[r], INPUT_PULLUP);
+	}
+
+	MsTimer2::set(1, sysTickHook);
+	MsTimer2::start();
+
+	Serial.begin(115200);
+
+	pif_Init();
+
+	pifLog_Init();
+	pifLog_AttachActPrint(_actLogPrint);
+
+    if (!pifPulse_Init(PULSE_COUNT)) return;
+    g_pstTimer1ms = pifPulse_Add(PULSE_ITEM_COUNT);
+    if (!g_pstTimer1ms) return;
+
+    pstKeypad = pifKeypad_Init(keys, ROWS, COLS);
+    if (!pstKeypad) return;
+    pstKeypad->actAcquire = _actKeypadAcquire;
+    pstKeypad->evtPressed = _evtKeypadPressed;
+    pstKeypad->evtReleased = _evtKeypadReleased;
+    pstKeypad->evtLongReleased = _evtKeypadLongReleased;
+    pstKeypad->evtDoublePressed = _evtKeypadDoublePressed;
+
+    if (!pifTask_Init(TASK_COUNT)) return;
+    if (!pifTask_AddPeriod(10, pifKeypad_LoopAll, NULL)) return;	// 10ms
+    if (!pifTask_AddPeriod(500, _actLedToggle, NULL)) return;		// 500ms
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+    pif_Loop();
+
+    pifTask_Loop();
+}
---
+++ arduino_nano/exKeypad1/exKeypad1.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _exKeypad1_H_
+#define _exKeypad1_H_
+#include "Arduino.h"
+//add your includes for the project exKeypad1 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project exKeypad1 here
+
+
+
+
+//Do not add code below this line
+#endif /* _exKeypad1_H_ */
