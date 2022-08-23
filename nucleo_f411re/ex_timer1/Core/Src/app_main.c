8e6332234bf433cb91b96a8abf738d2f5bd46f4e
跣辿 <joon0323@naver.com>
2022-08-23 21:38:20 +0900

    1. ex_timer1 예제 추가.

---
+++ nucleo_f411re/ex_timer1/Core/Src/app_main.c
@@ -0,0 +1,22 @@
+#include "app_main.h"
+#include "main.h"
+
+
+PifTimerManager g_timer_1ms;
+
+
+void appSetup()
+{
+	PifTimer *pstTimer1ms;
+
+	pif_Init(NULL);
+
+    if (!pifTaskManager_Init(1)) return;
+
+    if (!pifTimerManager_Init(&g_timer_1ms, PIF_ID_AUTO, 1000, 1)) return;	// 1000us
+
+    pstTimer1ms = pifTimerManager_Add(&g_timer_1ms, TT_REPEAT);
+    if (!pstTimer1ms) return;
+    pifTimer_AttachEvtFinish(pstTimer1ms, evtLedToggle, NULL);
+    pifTimer_Start(pstTimer1ms, 500);										// 500ms
+}
